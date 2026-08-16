a77f0dde6e5dc73655e28121da787ed5a4122c5c
Aschio12 <aschio12@users.noreply.github.com>
2026-08-16 16:20:56 +0300

    chore: configure vite proxy to route api requests to express backend

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,12 @@ export default defineConfig({
   server: {
     fs: {
       strict: false
+    },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      }
     }
   }
 })
